90f6baea50100b78b4384527d61bb5146e78f04a
Randy Rowland <rowland007@gmail.com>
2018-02-03 16:45:20 +0900

    Update ATM script to working

--- src/ATM.py
+++ src/ATM.py
@@ -6,6 +6,7 @@ Instructor:		Angel D. Cross
 Date:			1 Feb 2018
 Description:    The task for this project is to create a very simple ATM that addresses
                 developing functions and using regular expressions to search for patterns.
+Repository:     https://git.io/vNA9t
 Input:          stdin
 Output:         stdout
 Known bugs/missing features:
@@ -17,22 +18,48 @@ Date                Comment
 """
 import sys
 
-#account balance 
+# account balance
 account_balance = float(500.25)
 
 
-#<--------functions go here-------------------->
-#printbalance function
+# <--------functions go here-------------------->
+# print balance function
+def balance():
+  print("Your current balance:\n" + str(account_balance))
 
 
-#deposit function
+# deposit function
+def deposit():
+    global account_balance
+    depositAmount = raw_input("How much would you like to deposit today?\n")
+    account_balance += float(depositAmount)
+    print("Deposit was $" + str('%.2f' % float(depositAmount)) + ", current balance is $" + str(account_balance))
 
 
-#withdraw function
+# withdraw function
+def withdraw():
+    global account_balance
+    withdrawAmount = raw_input("How much would you like to withdraw today?\n")
+    if float(withdrawAmount) > account_balance:
+        print("$" + str('%.2f' % float(withdrawAmount)) + " is greater that your account balance of $" + str('%.2f' % float(account_balance)))
+    else:
+        account_balance -= float(withdrawAmount)
+        print("Withdrawal amount was $" + str('%.2f' % float(withdrawAmount)) + ", current balance is $" + str('%.2f' % float(account_balance)))
 
-#User Input goes here, use if/else conditional statement to call function based on user input
 
-userchoice = input ("What would you like to do?\n")
+# quit function
+def quit():
+    print("Thank you for banking with us.")
 
-if (userchoice == 'D'):
-    deposit ()
+
+# User Input goes here, use if/else conditional statement to call function based on user input
+
+userchoice = raw_input("What would you like to do?\n")
+
+options = {'B': balance,
+           'W': withdraw,
+           'D': deposit,
+           'Q': quit,
+}
+
+options[userchoice]()
